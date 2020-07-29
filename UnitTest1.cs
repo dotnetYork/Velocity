802920945f69a8a8f4f071e185ebb3eca50c2d37
Kevin Smith <kev_bite@msn.com>
2020-07-29 19:37:30 +0100

    Working

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Xunit;
 
 namespace Velocity
@@ -54,6 +56,27 @@ namespace Velocity
             int result = _tester.CountVelocityPeriods(1, 2, 3, 4);
             Assert.Equal(3, result);
         }
+
+        [Fact]
+        public void SixVelocitiesWithThreeRunsReturnsTen()
+        {
+            int result = _tester.CountVelocityPeriods(1, 2, 3, 4, 5, 6);
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void ThreeLongConstantRunsReturnCorrectResult()
+        {
+            int result = _tester.CountVelocityPeriods(1, 2, 3, 4, 5, 6, 1, 2, 3, 1, 2, 3, 4);
+            Assert.Equal(14, result);
+        }
+
+        [Fact]
+        public void ArrayLengthOf10000WithTwosReturns49985001()
+        {
+            int result = _tester.CountVelocityPeriods(Enumerable.Repeat(2, 10000).ToArray());
+            Assert.Equal(49985001, result);
+        }
     }
 
     public class VelocityTester
@@ -62,36 +85,49 @@ namespace Velocity
         {
             if (measurements.Length < 3) return 0;
 
+
             var currentDiff = measurements[1] - measurements[0];
             var currentCounter = 0;
             var total = 0;
-            for (var m = 1; m < measurements.Length-1; m++)
+            for (var m = 1; m < measurements.Length - 1; m++)
             {
                 var thisDiff = measurements[m + 1] - measurements[m];
                 if (thisDiff == currentDiff)
                 {
                     currentCounter++;
+                    total  +=  currentCounter;
                 }
                 else
                 {
-                    if (currentCounter == 1)
-                        total+=1;
+             //       total += AddCurrent(currentCounter);
 
-                    if (currentCounter == 2)
-                        total += 3;
-
-                    currentCounter  =  0;
+                    currentCounter = 0;
                     currentDiff = thisDiff;
                 }
             }
 
-            if (currentCounter == 1)
-                total += 1;
-
-            if (currentCounter == 2)
-                total += 3;
+     //       total += AddCurrent(currentCounter);
 
             return total;
+
+            //                             *    1 2*3
+            // Length 2 => 3              * *   1 2*3*4
+            // Length 3 => 6             * * *
+            // Length 4 => 10           * * * *
+            // Length 5 => 10         * * * * * *
+
+        }
+
+        private int AddCurrent(int currentCounter)
+        {
+            return currentCounter  *  (currentCounter  +  1)  /  2;
+            //var total = 0;
+            //for (int i = 1; i <= currentCounter; i++)
+            //    total += i;
+            //return total;
+
+            // T = (n)(n + 1) / 2
+            //(n / 2)(first number + last number)
         }
     }
 }
